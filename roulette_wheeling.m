262caa383dc5960e6890274f334b3b24a8b824e4
Rahul Verma <53015177+master-rahul@users.noreply.github.com>
2019-11-24 15:34:36 +0530

    Update roulette_wheeling.m

--- roulette_wheeling.m
+++ roulette_wheeling.m
@@ -1,15 +1,11 @@
 function ii=roulette_wheeling(m,prn)
 cumulative_sum=cumsum(prn);
-
 n=length(prn);
-
-
 sz=size(cumulative_sum);
 if sz(1)>sz(2)
     cumulative_sum1=cumulative_sum';
 else
     cumulative_sum1=cumulative_sum;
 end
-
 i1=interp1([0 cumulative_sum1],1:(n+1),rand(1,m),'linear');
 ii=floor(i1); 
